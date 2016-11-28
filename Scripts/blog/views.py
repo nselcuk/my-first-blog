ca697456f470aa61387acf2e771b46f6e3296a44
nselcuk <nr.selcuk@gmail.com>
2016-11-28 14:14:27 +0300

    Including main page / HTML change

--- Scripts/blog/views.py
+++ Scripts/blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
+# 2 seperate definition here
+
+class PostListView(TemplateView):
+    template_name = "blog/post_list.html"
+
+# def post_list(request):
+   # return render(request, 'blog/post_list.html', {})
